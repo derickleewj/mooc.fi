040634777d5dfd2edfbbb30db235690652bae519
Derick <dericklee@outlook.sg>
2022-04-20 17:34:02 +0800

    Update

---
+++ Java_1_Part4/part04-Part04_15.NumberStatistics/src/main/java/MainProgram.java
@@ -0,0 +1,43 @@
+
+import java.util.Scanner;
+
+public class MainProgram {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Statistics sum = new Statistics();
+        Statistics even = new Statistics();
+        Statistics odd = new Statistics();
+        
+        System.out.println("Enter numbers:");
+        
+        while(true) {
+            int input = Integer.valueOf(scanner.nextLine());
+        
+        if (input == -1) {
+            System.out.println("Sum: " + sum.sum());
+            System.out.println("Sum of even numbers: " + even.sum());
+            System.out.println("Sum of odd numbers: " + odd.sum());
+            break;
+            } else {
+            sum.addNumber(input);
+            
+            if (input % 2 == 0) {
+                even.addNumber(input);
+            } else {
+                odd.addNumber(input);
+            } // end of if statement
+            
+            } // end of while statement
+        }
+        
+        
+        // you can write test code here
+        // however, remove all unnecessary code when doing the final parts of the exercise
+
+        // In order for the tests to work, the objects must be created in the
+        // correct order in the main program. First the object that tracks the total
+        // sum, secondly the object that tracks the sum of even numbers, 
+        // and lastly the one that tracks the sum of odd numbers!
+    }
+}
